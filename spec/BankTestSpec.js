f5d81a8aef2b156842c51a5d6412cb9a514c971d
carlton anand <carlton.anand@gmail.com>
2019-07-01 16:13:31 +0100

    adding ability for withdraw to add to the transactions record, updated tests

--- spec/BankTestSpec.js
+++ spec/BankTestSpec.js
@@ -33,28 +33,27 @@ describe('Bank', function() {
     var dd = today.getDate();
     var mm = today.getMonth()+1; //January is 0!
     var yyyy = today.getFullYear();
-
-    if(dd<10) {
-        dd = '0'+dd
-    } 
-
-    if(mm<10) {
-        mm = '0'+mm
-    } 
-
+    if(dd<10) {dd = '0'+dd}
+    if(mm<10) {mm = '0'+mm} 
     today = dd + '/' + mm + '/' + yyyy;
 
     it("should display transaction list headers - date,credit,debit and balance", function(){
       bank._statement;
-      expect(bank._statement).toEqual(["date || credit || debit || balance"])
+      expect(bank._transactions).toEqual(["date || credit || debit || balance"])
     });
 
     it("should display a deposit transaction", function(){
       bank.deposit(1000);
     
-      expect(bank._statement).toEqual(["date || credit || debit || balance", `${today} || 1000 || || 1000`])
+      expect(bank._transactions).toEqual(["date || credit || debit || balance", `${today} || 1000 || || 1000`])
     });
 
+    it("should display a withdraw transaction", function(){
+      bank.deposit(1000);
+      bank.withdraw(250);
+    
+      expect(bank._transactions).toEqual(["date || credit || debit || balance", `${today} || || 250 || 750`, `${today} || 1000 || || 1000`])
+    });
     
   });
 
--- src/BankTest.js
+++ src/BankTest.js
@@ -2,35 +2,27 @@
 
 function Bank() {
   this._balance = 0;
-  this._statement = ['date || credit || debit || balance']
+  this._transactions = ['date || credit || debit || balance']
 };
 
 var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-
-    if(dd<10) {
-        dd = '0'+dd
-    } 
-
-    if(mm<10) {
-        mm = '0'+mm
-    } 
-
-    today = dd + '/' + mm + '/' + yyyy;
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+if(dd<10) {dd = '0'+dd}
+if(mm<10) {mm = '0'+mm} 
+today = dd + '/' + mm + '/' + yyyy;
 
 Bank.prototype.deposit = function(money) {
   this._balance = this._balance + money;
   var record = `${today} || ${money} || || ${this._balance}`
-  this._statement.splice(1, 0, record);
+  this._transactions.splice(1, 0, record);
 };
 
 Bank.prototype.withdraw = function(money) {
   this._balance = this._balance - money;
+  var record = `${today} || || ${money} || ${this._balance}`
+  this._transactions.splice(1, 0, record);
 };
 
-// Bank.prototype.bankStatement = function() {
-//   console.log(6)
-// };
 
