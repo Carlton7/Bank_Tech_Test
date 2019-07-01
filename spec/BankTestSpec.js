03b29f2106c094b345492fa9a97f1c8419f5a128
carlton anand <carlton.anand@gmail.com>
2019-07-01 15:39:53 +0100

    added statement variable and tests. changed the deposit function to send a record to this to keep a log

--- spec/BankTestSpec.js
+++ spec/BankTestSpec.js
@@ -5,13 +5,14 @@ describe('Bank', function() {
 
   beforeEach(function() {
     bank = new Bank();
+    
   });
 
   it('should start with a £0 balance', function() {
     expect(bank._balance).toEqual(0);
   });
 
-  describe("'Deposit' method", function() {
+  describe("'deposit' method", function() {
     it("should increase balance by £1000", function() {
       bank.deposit(1000);
       expect(bank._balance).toEqual(1000);
@@ -24,6 +25,37 @@ describe('Bank', function() {
       bank.withdraw(500);
       expect(bank._balance).toEqual(500);
     });
+  });
+
+  describe("bank statement method", function() {
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+
+    today = dd + '/' + mm + '/' + yyyy;
+
+    it("should display transaction list headers - date,credit,debit and balance", function(){
+      bank._statement;
+      expect(bank._statement).toEqual(["date || credit || debit || balance"])
+    });
+
+    it("should display a deposit transaction", function(){
+      bank.deposit(1000);
+    
+      expect(bank._statement).toEqual(["date || credit || debit || balance", `${today} || 1000 || || 1000`])
+    });
+
+    
   });
 
 });
