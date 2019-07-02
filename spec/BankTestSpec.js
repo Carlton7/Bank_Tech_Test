0a7eaa4e9401ee147e4a9b38394da0dd1c2139f3
carlton anand <carlton.anand@gmail.com>
2019-07-02 12:23:12 +0100

    further refactoring and using jslint on the code, date variable changed

--- spec/BankTestSpec.js
+++ spec/BankTestSpec.js
@@ -4,18 +4,17 @@ describe('Bank', function() {
   var bank;
 
   beforeEach(function() {
-    bank = new Bank();
-    
+    bank = new Bank(); 
   });
 
   it('should start with a £0 balance', function() {
-    expect(bank._balance).toEqual(0);
+    expect(bank.balance).toEqual(0);
   });
 
   describe("'deposit' method", function() {
     it("should increase balance by £1000", function() {
       bank.deposit(1000);
-      expect(bank._balance).toBeCloseTo(1000.00, 2);
+      expect(bank.balance).toBeCloseTo(1000.00, 2);
     });
   });
 
@@ -23,38 +22,31 @@ describe('Bank', function() {
     it("should decrease the balance by £500", function() {
       bank.deposit(1000);
       bank.withdraw(500);
-      expect(bank._balance).toBeCloseTo(500.00, 2);
+      expect(bank.balance).toBeCloseTo(500.00, 2);
     });
   });
 
   describe("bank statement method", function() {
 
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-    if(dd<10) {dd = '0'+dd}
-    if(mm<10) {mm = '0'+mm} 
-    today = dd + '/' + mm + '/' + yyyy;
+    const movDate = new Date();
+    var date = (`${movDate.getDate()}/${movDate.getMonth() + 1}/${movDate.getFullYear()}`)
 
     it("should display transaction list headers - date,credit,debit and balance", function(){
-      bank._statement;
-      expect(bank._transactions).toEqual(["date || credit || debit || balance"])
+      bank.transactions;
+      expect(bank.transactions).toEqual(["date || credit || debit || balance"])
     });
 
     it("should display a deposit transaction", function(){
       bank.deposit(1000);
     
-      expect(bank._transactions).toEqual(["date || credit || debit || balance", `${today} || 1000.00 || || 1000.00`])
+      expect(bank.transactions).toEqual(["date || credit || debit || balance", `${date} || 1000.00 || || 1000.00`])
     });
 
     it("should display a withdraw transaction", function(){
       bank.deposit(1000);
       bank.withdraw(250);
     
-      expect(bank._transactions).toEqual(["date || credit || debit || balance", `${today} || || 250.00 || 750.00`, `${today} || 1000.00 || || 1000.00`])
-    });
-    
+      expect(bank.transactions).toEqual(["date || credit || debit || balance", `${date} || || 250.00 || 750.00`, `${date} || 1000.00 || || 1000.00`])
+    });   
   });
-
 });
--- src/BankTest.js
+++ src/BankTest.js
@@ -1,32 +1,27 @@
-'use strict';
+"use strict";
 
 function Bank() {
-  this._balance = 0.00;
-  this._transactions = ['date || credit || debit || balance']
-};
+  this.balance = 0.00;
+  this.transactions = ["date || credit || debit || balance"];
+}
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
-if(dd<10) {dd = '0'+dd}
-if(mm<10) {mm = '0'+mm} 
-today = dd + '/' + mm + '/' + yyyy;
+const movDate = new Date();
+var date = (`${movDate.getDate()}/${movDate.getMonth() + 1}/${movDate.getFullYear()}`)
 
 Bank.prototype.deposit = function(money) {
-  this._balance = this._balance + money;
-  var record = `${today} || ${money.toFixed(2)} || || ${this._balance.toFixed(2)}`
-  this._transactions.splice(1, 0, record);
+  this.balance = this.balance + money;
+  var record = `${date} || ${money.toFixed(2)} || || ${this.balance.toFixed(2)}`;
+  this.transactions.splice(1, 0, record);
 };
 
 Bank.prototype.withdraw = function(money) {
-  this._balance = this._balance - money;
-  var record = `${today} || || ${money.toFixed(2)} || ${this._balance.toFixed(2)}`
-  this._transactions.splice(1, 0, record);
+  this.balance = this.balance - money;
+  var record = `${date} || || ${money.toFixed(2)} || ${this.balance.toFixed(2)}`;
+  this.transactions.splice(1, 0, record);
 };
 
 Bank.prototype.bankStatement = function() {
-  this._transactions.forEach(function(element) {
+  this.transactions.forEach(function(element) {
     console.log(element);
   });
 };
